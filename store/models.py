00466a9645d44c15ed883fd065e66dd9fab78c6f
wasif-amin <aminwasif96@gmail.com>
2026-09-17 12:27:58 -0400

    Update Product model to match img field to prop name in frontend

--- store/models.py
+++ store/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
-    image_url = models.CharField(max_length=500, blank=True, null=True)
+    img = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
