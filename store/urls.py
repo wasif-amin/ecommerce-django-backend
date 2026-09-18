0979d14e58aa8b06f41b704376cbdb12f12965d5
wasif-amin <aminwasif96@gmail.com>
2026-09-18 15:51:17 -0400

    Remove .DS_Store files from tracking

---
+++ store/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import product_list_api
+
+urlpatterns = [
+    path('products/', product_list_api, name='product_list_api'),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+# your_app_name/views.py
+from django.http import JsonResponse
+from .models import Product
+
+def product_list_api(request):
+    # 1. Fetch the data
+    all_products = Product.objects.all()
+    
+    # 2. Convert the database rows into a Python list of dictionaries
+    product_data = list(all_products.values('id', 'name', 'price', 'stock', 'img'))
+    print(product_data)
+    # 3. Return the data as JSON
+    return JsonResponse(product_data, safe=False)
+
