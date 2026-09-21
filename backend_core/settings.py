c1737c7584fbae3575999d64b4342a96b24922f4
wasif-amin <aminwasif96@gmail.com>
2026-09-21 11:10:49 -0400

    Fetch products from backend into the frontend

--- backend_core/settings.py
+++ backend_core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'store',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend_core.urls'
@@ -98,8 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+CORS_ALLOW_CREDENTIALS = True
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./Navbar";
 import Login from "./Login";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5001";
+  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 function App() {
   const [isAdmin, setIsAdmin] = useState(false);
@@ -219,7 +219,7 @@ function App() {
   }
 
   useEffect(() => {
-    fetch(`${API_BASE_URL}/api/products`)
+    fetch(`http://127.0.0.1:8000/api/products/`)
       .then((res) => res.json())
       .then((data) => setProducts(data))
       .catch((err) => console.error("Error fetching products:", err));
