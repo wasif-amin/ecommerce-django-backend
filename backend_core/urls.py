d06b2832962c155e97159f3340bf915580840562
wasif-amin <aminwasif96@gmail.com>
2026-09-24 10:25:26 -0400

    Register Product model to views.py to enable CRUD controls on Django admin panel

--- backend_core/urls.py
+++ backend_core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic.base import RedirectView 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('store.urls')), 
+    path('api/', include('store.urls')),
+    path('', RedirectView.as_view(url='http://localhost:5173/', permanent=False)) 
 ]
 #
